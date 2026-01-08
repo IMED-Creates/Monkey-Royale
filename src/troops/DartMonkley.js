3bfdc3b20417c0706126605256f08cf263cc5dfe
IMED-Creates <ethan.lam.chen10@gmail.com>
2026-01-07 18:29:29 -0800

    Add DartMonkey class with initial properties

---
+++ src/troops/DartMonkley.js
@@ -0,0 +1,9 @@
+export class DartMonkey {
+  constructor() {
+    this.name = "Dart Monkey";
+    this.cost = 3; // bananas
+    this.hp = 100;
+    this.damage = 20;
+    this.attackSpeed = 1; // attacks per second
+  }
+}
